50fa3cb6a20eecb22a1270b4e2a65dbe1f3ad9ba
muchomucho-tounyu <ckcgbisbm@icloud.com>
2025-07-09 05:59:12 +0000

    cssMaking

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                kawaii: ['"Zen Kaku Gothic New"', "sans-serif"],
+                //sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
     },

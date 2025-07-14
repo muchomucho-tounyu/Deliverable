a08d2dc5dadfd173392a4b0ebcb180576a6e442a
muchomucho-tounyu <ckcgbisbm@icloud.com>
2025-07-14 04:22:47 +0000

    image

--- config/services.php
+++ config/services.php
@@ -39,6 +39,7 @@ return [
         'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
         'api_key'    => env('CLOUDINARY_API_KEY'),
         'api_secret' => env('CLOUDINARY_API_SECRET'),
+        'url'        => env('CLOUDINARY_URL'),
     ],
 
 ];
